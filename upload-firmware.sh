04eed668204e229d678f041af09a1822f3d6880d
Putta Khunchalee <ultimaweapon@outlook.com>
2017-06-21 12:08:27 +0700

    Add script to upload firmware from PX4 project

---
+++ upload-firmware.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+port=$1
+if [ -z "$port" ]; then
+  echo "Usage: $0 PORT"
+  exit 1
+fi
+
+./px_uploader.py --port "$port" --baud-bootloader 115200 --baud-flightstack 115200 PlayuavOSD.hex
